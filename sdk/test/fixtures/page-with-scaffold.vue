65c0937dd60e70a07557d1e8df087adf901f2ea9
Richard Lund <richard.lund@crownpeak.com>
2020-12-29 16:58:06 +0000

    Unit tests for cp-scaffold on pages.

---
+++ sdk/test/fixtures/page-with-scaffold.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-if="isLoaded">
+        <h1><SimpleComponent /></h1>
+        <p>Before</p>
+        <!-- cp-scaffold 
+        <h2>{Heading:Text}</h2>
+        else -->
+        <h2>{ "not present" }</h2>
+        <!-- /cp-scaffold -->
+        <p>Between</p>
+        <!-- cp-scaffold 
+        {SupplementaryField:Text}
+        /cp-scaffold -->
+        <p>After</p>
+    </div>
+</template>
+<script>
+    import Vue from 'vue';
+    import { CmsStaticPage } from 'crownpeak-dxm-vuejs-sdk';
+    import SimpleComponent from './simple-component';
+
+    export default {
+        extends: CmsStaticPage,
+        name: "PageWithScaffold",
+        components: {
+            SimpleComponent
+        }
+    };
+</script>
---
+++ sdk/test/page-with-scaffold.test.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+const parser = require('../classes/parsers/page');
+const fs = require('fs');
+const path = require('path');
+
+const file = path.resolve('./test/fixtures/page-with-scaffold.vue');
+const content = fs.readFileSync(file, 'utf8');
+const { pages, uploads } = parser.parse(content, file);
+
+describe('Page With Scaffold', () => {
+    it('should find no uploads', () => {
+        assert.strictEqual(uploads.length, 0);
+    });
+    it('should find one page', () => {
+        assert.strictEqual(pages.length, 1);
+        assert.strictEqual(pages[0].name, "PageWithScaffold");
+        assert.strictEqual(pages[0].content, "<div>\r\n    <h1>{SimpleComponent}</h1>\r\n    <p>Before</p>\r\n    <h2>{Heading:Text}</h2>\r\n    <p>Between</p>\r\n    {SupplementaryField:Text}\r\n    <p>After</p>\r\n</div>\r\n");
+    });
+});
